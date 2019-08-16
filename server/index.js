40429f16a5c1ac3f0edddab08529e6eb46cb2f3e
Barry Huang <Barry.huang93@gmail.com>
2019-08-16 15:33:18 -0400

    inital commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '..', 'dist')))
+
+
+app.use('*', (req,res,next)=> {
+  res.sendFile(path.join(__dirname, '..', 'dist/index.html'))
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+  console.log(`app is running on PORT ${port}`);
+})
