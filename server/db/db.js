a3657c58441c18e8371871387db4b472351e73d7
barry huang <barry.huang93@gmail.com>
2020-01-02 12:11:47 -0500

    db and sequelize added

---
+++ server/db/db.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
+
+const databasename = pkg.name
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databasename}`, { logging: false, }
+)
+
+module.exports = db
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const db = require('./db')
+
+db.sync()
 
 app.use(express.static(path.join(__dirname, '..', 'dist')))
 
